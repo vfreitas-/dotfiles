00ca4891419169198f541676a76a3ec9dc0aa395
vfreitas- <vitor_freitas_b@hotmail.com>
2020-08-04 23:21:09 -0300

    feat: add zsh theme

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@
 # setup oh-my-zsh
 echo "Installing Oh My ZSH"
 curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
+# install theme
+git clone https://github.com/dracula/zsh.git temp-dracula
+cp ~/temp-dracula/dracula.zsh-theme ~/.oh-my-zsh/themes 
+cp -r ~/temp-dracula/lib ~/.oh-my-zsh/themes
